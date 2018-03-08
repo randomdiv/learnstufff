3bba53063dc163aabda201a996486985074e3680
Ahmed Abdelhadi <e-tools@live.com>
2018-03-08 21:48:24 +0100

    Added category primary logic

--- routes/categories.js
+++ routes/categories.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router  = express.Router();
 const db      = require('../models');
 const main    = require('../helpers/main');
+
+//getting all categories
+router.get("/", (req, res)=>{
+  db.Category.find({})
+    .populate("courses")
+    .exec
+    .then(categories=>{
+      res.json(categories);
+    })
+    .catch(error=>{
+      res.send(error);
+    });
+});
 // adding a new category
 router.post('/', (req, res)=>{
   let newCat = req.body;
@@ -12,6 +25,26 @@ router.post('/', (req, res)=>{
   .catch(error=>{
     res.send(error);
   });
+});
+//edit a category
+router.put("/:id/edit", (req, res) =>{
+  db.Category.findByIdAndUpdate(req.params.id, req.body)
+    .then(updatedCategory=>{
+      res.json(updatedCategory);
+    })
+    .catch(error =>{
+      res.send(error);
+    });
+});
+//delete category
+router.delete("/:id", (req, res) =>{
+  db.Category.findByIdAndRemove(req.params.id)
+    .then(()=>{
+      res.json({message:"Category has been deleted!"});
+    })
+    .catch(error=>{
+      res.send(error);
+    });
 });
 
 module.exports = router;
